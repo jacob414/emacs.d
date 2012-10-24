9c2aa5eb060833f2827eb2a68febf37d89da7bbe
Jacob Oscarson <jacob@plexical.com>
2012-10-24 13:25:08 +0200

    Moved out custom keybindings to it's own module

--- dot-emacs.el
+++ dot-emacs.el
@@ -6,28 +6,7 @@
 ;; Basic environment ----------------------------------------------------------
 
 (require 'baseenv)
-
-;; Always indent
-(global-set-key (kbd "C-m") 'newline-and-indent)
-
-;; Custom key bindings
-(global-set-key (kbd "C-<tab>") 'dabbrev-expand)
-(global-set-key (kbd "C-z") 'goto-line)
-(global-set-key (kbd "C-c j") 'goto-line)
-(global-set-key (kbd "C-c C-j") 'goto-line)
-(global-set-key (kbd "C-(") 'insert-parentheses)
-(global-set-key (kbd "C-c i") 'indent-region)
-(global-set-key (kbd "<f5>") 'eval-region)
-(global-set-key (kbd "C-c p") 'query-replace)
-(global-set-key (kbd "C-c a") 'mark-whole-buffer)
-(global-set-key (kbd "C-c f") 'rgrep)
-(global-set-key (kbd "C-c o") 'occur)
-(global-set-key (kbd "C-c k") 'kill-whole-line)
-(global-set-key (kbd "C-c m t") 'ws-trim-mode)
-(global-set-key (kbd "C-c m l") 'longlines-mode)
-(global-set-key (kbd "C-x <SPC>") 'whitespace-mode)
-(global-set-key (kbd "C-c T") 'ws-trim-buffer)
-(global-set-key (kbd "C-c T") 'ws-trim-buffer)
+(require 'j414-custom-keybindings)
 
 ;; Line numbers to the left
 (require 'linum)
---
+++ j414-custom-keybindings.el
@@ -0,0 +1,23 @@
+;; Always indent
+(global-set-key (kbd "C-m") 'newline-and-indent)
+
+;; Custom key bindings
+(global-set-key (kbd "C-<tab>") 'dabbrev-expand)
+(global-set-key (kbd "C-z") 'goto-line)
+(global-set-key (kbd "C-c j") 'goto-line)
+(global-set-key (kbd "C-c C-j") 'goto-line)
+(global-set-key (kbd "C-(") 'insert-parentheses)
+(global-set-key (kbd "C-c i") 'indent-region)
+(global-set-key (kbd "<f5>") 'eval-region)
+(global-set-key (kbd "C-c p") 'query-replace)
+(global-set-key (kbd "C-c a") 'mark-whole-buffer)
+(global-set-key (kbd "C-c f") 'rgrep)
+(global-set-key (kbd "C-c o") 'occur)
+(global-set-key (kbd "C-c k") 'kill-whole-line)
+(global-set-key (kbd "C-c m t") 'ws-trim-mode)
+(global-set-key (kbd "C-c m l") 'longlines-mode)
+(global-set-key (kbd "C-x <SPC>") 'whitespace-mode)
+(global-set-key (kbd "C-c T") 'ws-trim-buffer)
+(global-set-key (kbd "C-c T") 'ws-trim-buffer)
+
+(provide 'j414-custom-keybindings)
