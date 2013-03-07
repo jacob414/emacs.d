13f3ec313cf9027644792773313de463ad56db01
Jacob Oscarson <jacob@plexical.com>
2013-03-07 15:57:59 +0100

    No idea why that didn't work

--- init.el
+++ init.el
@@ -1,8 +1,7 @@
 ;; Load path settings
-(setq emacs-dir (file-name-directory
-                    (or (buffer-file-name) load-file-name)))
-(add-to-list 'load-path emacs-dir)
+(setq emacs-dir "~/src/mine/emacs.d")
 
+(add-to-list 'load-path emacs-dir)
 (add-to-list 'load-path (concat emacs-dir "/site-lisp"))
 (add-to-list 'load-path (concat emacs-dir "/hosts"))
 
