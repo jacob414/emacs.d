05056bf07f8cca8fd6d8f4b40d994c44ffeff0e6
Jacob Oscarson <jacob.oscarson@glooko.com>
2017-06-30 11:44:48 +0200

    Added Homebrew binary path to OS X support

--- functions.el
+++ functions.el
@@ -180,6 +180,7 @@
   (global-set-key (kbd "M-ä") "”")
   (global-set-key (kbd "<end>") 'end-of-line)
   (global-set-key (kbd "<home>") 'beginning-of-line)
+  (setq exec-path (append exec-path '("/usr/local/bin")))
   )
 
 (defun my-longlines ()
