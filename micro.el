7b6ed42a31080e0d57b6f6456135d1e912447062
Jacob Oscarson <jacob.oscarson@glooko.com>
2018-08-03 12:35:33 +0200

    Linum in my (newish) CLI emacs

--- micro.el
+++ micro.el
@@ -32,3 +32,17 @@
 
 (require 'warnings)
 (add-to-list 'warning-suppress-types '(yasnippet backquote-change))
+
+;; linum ----------------------------------------------------------------------
+
+;; Line numbers to the left
+(require 'linum)
+(global-linum-mode t)
+(setq linum-format "%d ")
+
+(custom-set-faces
+ '(linum ((t
+           (:inherit (shadow default)
+                     :foreground "dark orange"
+                     :background "black"
+                     ))) ))
