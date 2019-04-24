ff142dce49c97b3f36bf4d355d17d693110132be
Jacob Oscarson <jacob.oscarson@glooko.com>
2019-04-24 13:30:03 +0200

    Added found rename current buffer file function

--- functions.el
+++ functions.el
@@ -252,4 +252,29 @@
 
 (global-set-key (kbd "C-x a") 'my-archive)
 
+(defun rename-current-buffer-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let* ((name (buffer-name))
+         (filename (buffer-file-name))
+         (basename (file-name-nondirectory filename)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let
+          ((new-name
+            (read-file-name "New name: "
+                            (file-name-directory filename)
+                            basename nil basename)))
+        (if (get-buffer new-name)
+            (error "A buffer named '%s' already exists!" new-name)
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil)
+          )
+        )
+      )
+    )
+  )
+
 (provide 'functions)
