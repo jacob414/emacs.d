ee3e0e67edba827d71ee5c34a7b44592b9947435
Jacob Oscarson <jacob@414soft.com>
2019-10-25 14:50:32 +0200

    Gather org-mode stuff in one module

--- init.el
+++ init.el
@@ -302,84 +302,9 @@
 
 ;; org-mode settings  ---------------------------------------------------------
 
-;; #+LaTeX_CLASS: beamer in org files
-(unless (boundp 'org-export-latex-classes)
-  (setq org-export-latex-classes nil))
-(add-to-list 'org-export-latex-classes
-  ;; beamer class, for presentations
-  '("beamer"
-     "\\documentclass[11pt]{beamer}\n
-      \\mode<{{{beamermode}}}>\n
-      \\usetheme{{{{beamertheme}}}}\n
-      \\usecolortheme{{{{beamercolortheme}}}}\n
-      \\beamertemplateballitem\n
-      \\setbeameroption{show notes}
-      \\usepackage[utf8]{inputenc}\n
-      \\usepackage[T1]{fontenc}\n
-      \\usepackage{hyperref}\n
-      \\usepackage{color}
-      \\usepackage{listings}
-      \\lstset{numbers=none,language=[ISO]C++,tabsize=4,
-  frame=single,
-  basicstyle=\\small,
-  showspaces=false,showstringspaces=false,
-  showtabs=false,
-  keywordstyle=\\color{blue}\\bfseries,
-  commentstyle=\\color{red},
-  }\n
-      \\usepackage{verbatim}\n
-      \\institute{{{{beamerinstitute}}}}\n
-       \\subject{{{{beamersubject}}}}\n"
-
-     ("\\section{%s}" . "\\section*{%s}")
-
-     ("\\begin{frame}[fragile]\\frametitle{%s}"
-       "\\end{frame}"
-       "\\begin{frame}[fragile]\\frametitle{%s}"
-       "\\end{frame}")))
-
-  ;; letter class, for formal letters
-
-  (add-to-list 'org-export-latex-classes
-
-  '("letter"
-     "\\documentclass[11pt]{letter}\n
-      \\usepackage[utf8]{inputenc}\n
-      \\usepackage[T1]{fontenc}\n
-      \\usepackage{color}"
-
-     ("\\section{%s}" . "\\section*{%s}")
-     ("\\subsection{%s}" . "\\subsection*{%s}")
-     ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-     ("\\paragraph{%s}" . "\\paragraph*{%s}")
-     ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
-
-;; http://orgmode.org/manual/Conflicts.html
-(add-hook 'org-mode-hook
-          (lambda ()
-            (local-set-key (kbd "M-<up>") 'windmove-up)
-            (local-set-key (kbd "M-<left>") 'windmove-left)
-            (local-set-key (kbd "M-<right>") 'windmove-right)
-            (local-set-key (kbd "M-<down>") 'windmove-down)
-            (local-set-key (kbd "C-<tab>") 'dabbrev-expand)
-            (local-set-key (kbd "C-.") 'scroll-down-one-line)
-            (local-set-key (kbd "C-,") 'scroll-up-one-line)
-            (local-set-key (kbd "C-j") 'my-greedy-joinlines)
-            (define-key yas/keymap [tab] 'yas/next-field-or-maybe-expand)))
-;; [return tab backspace backspace ?- ?  ?\M-8 ?  ?\M-9 ? ]
-
-
-
-(setq org-file-apps
-    '(("\\.docx?\\'" . default)
-      ("\\.xlsx?\\'" . default)
-      ("\\.x?html?\\'" . default)
-      ("\\.pdf\\'" . default)
-      ("\\.log\\'" . emacs)
-      (".*::\\(editme\\)\\'" . (find-file file))
-      (auto-mode . emacs)))
-
-;; (setq org-src-fontify-natively t)
+(message "org?")
+(require 'my-org)
+(message "org ok")
 
 ;; expand-region --------------------------------------------------------------
 
