dbbcfb810b23aa4ae784b204f3b1f8461d335031
Jacob Oscarson <jacob@414soft.com>
2025-09-19 13:36:56 +0200

    New feature to switch Virtualenv when entering Python buffers with a ./.venv directory beside them

--- my-python.el
+++ my-python.el
@@ -1,9 +1,54 @@
 ;; Python settings ------------------------------------------------------------
 
-(setq my/venv (expand-file-name "~/opt/plus/def-venv"))
-(setq my/python-bin (expand-file-name "~/opt/plus/def-venv/bin/python"))
-(setq my/pytest-bin (expand-file-name "~/opt/plus/def-venv/bin/pytest"))
-(setq my/mypy-bin (expand-file-name "~/opt/plus/def-venv/bin/mypy"))
+;; Default virtualenv configuration
+(setq my/default-venv (expand-file-name "~/opt/plus/def-venv"))
+(setq my/current-venv my/default-venv)
+(setq my/last-buffer-dir nil)
+
+;; Function to find project-local .venv directory
+(defun my/find-project-venv (start-dir)
+  "Find .venv directory by walking up from START-DIR."
+  (when start-dir
+    (let ((venv-path (expand-file-name ".venv" start-dir)))
+      (cond
+       ((file-directory-p venv-path) venv-path)
+       ((string= start-dir "/") nil)
+       (t (my/find-project-venv (file-name-directory (directory-file-name start-dir))))))))
+
+;; Function to update Python paths for current venv
+(defun my/update-python-paths (venv-path)
+  "Update all Python-related paths for VENV-PATH."
+  (setq my/current-venv venv-path
+        my/python-bin (expand-file-name "bin/python" venv-path)
+        my/pytest-bin (expand-file-name "bin/pytest" venv-path)
+        my/mypy-bin (expand-file-name "bin/mypy" venv-path)
+        python-shell-interpreter my/python-bin
+        elpy-rpc-python-command my/python-bin
+        elpy-rpc-virtualenv-path venv-path
+        elpy-syntax-check-command my/mypy-bin
+        elpy-test-pytest-runner-command my/pytest-bin
+        python-check-command my/mypy-bin))
+
+;; Auto-switch virtualenv based on buffer location
+(defun my/auto-switch-venv ()
+  "Automatically switch virtualenv based on current buffer's directory."
+  (when (and buffer-file-name
+             (derived-mode-p 'python-mode))
+    (let* ((buffer-dir (file-name-directory buffer-file-name))
+           (project-venv (my/find-project-venv buffer-dir)))
+      (when (not (string= buffer-dir my/last-buffer-dir))
+        (setq my/last-buffer-dir buffer-dir)
+        (let ((target-venv (or project-venv my/default-venv)))
+          (when (not (string= target-venv my/current-venv))
+            (my/update-python-paths target-venv)
+            (pyvenv-activate target-venv)
+            (message "Switched to venv: %s"
+                     (if project-venv
+                         (file-name-nondirectory (directory-file-name project-venv))
+                       "default"))))))))
+
+;; Set initial paths
+(my/update-python-paths my/default-venv)
 
 (require 'flymake)
 (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)
@@ -36,19 +81,40 @@
 (setq elpy-modules '(elpy-module-company elpy-module-eldoc elpy-module-flymake
                                          elpy-module-pyvenv elpy-module-highlight-indentation
                                          elpy-module-yasnippet elpy-module-sane-defaults)
-      python-shell-interpreter my/python-bin
-      elpy-rpc-python-command my/python-bin
-      elpy-rpc-virtualenv-path my/venv
-      elpy-syntax-check-command my/mypy-bin
       elpy-test-discover-runner-command '("python" "-m" "pytest")
-      elpy-test-pytest-runner-command my/pytest-bin
-      python-check-command my/mypy-bin
       elpy-test-runner 'elpy-test-pytest-runner)
 
 (setq comint-process-echoes t)
 
-(pyvenv-activate my/venv)
-(pyvenv-workon my/venv)
+;; Activate default venv on startup
+(pyvenv-activate my/default-venv)
+(pyvenv-workon my/default-venv)
+
+;; Hook to automatically switch venv when changing buffers
+(add-hook 'buffer-list-update-hook #'my/auto-switch-venv)
+
+;; Manual command to check current venv and force switch
+(defun my/show-current-venv ()
+  "Show current virtualenv and project detection status."
+  (interactive)
+  (let* ((buffer-dir (when buffer-file-name (file-name-directory buffer-file-name)))
+         (project-venv (when buffer-dir (my/find-project-venv buffer-dir)))
+         (venv-name (if (string= my/current-venv my/default-venv)
+                        "default"
+                      (file-name-nondirectory (directory-file-name my/current-venv)))))
+    (message "Current venv: %s | Buffer dir: %s | Project venv: %s"
+             venv-name
+             (or buffer-dir "none")
+             (if project-venv
+                 (file-name-nondirectory (directory-file-name project-venv))
+               "none"))))
+
+(defun my/force-venv-switch ()
+  "Force virtualenv switch check for current buffer."
+  (interactive)
+  (setq my/last-buffer-dir nil)
+  (my/auto-switch-venv)
+  (my/show-current-venv))
 
 (defun my-mypy ()
   "Docstring for my-mypy."
